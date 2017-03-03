fd5e092d7371116ade08da6d344f5c076d575207
blackmatch <coder.blackmatch@gmail.com>
2017-03-03 11:29:21 +0800

    first commit

---
+++ md5Test/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace md5Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.cryptBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton32 = new System.Windows.Forms.RadioButton();
+            this.radioButton16 = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.radioButtonUp = new System.Windows.Forms.RadioButton();
+            this.radioButtonLow = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "输入字符串：";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(121, 66);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(232, 21);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // cryptBtn
+            // 
+            this.cryptBtn.Location = new System.Drawing.Point(190, 219);
+            this.cryptBtn.Name = "cryptBtn";
+            this.cryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.cryptBtn.TabIndex = 2;
+            this.cryptBtn.Text = "加密";
+            this.cryptBtn.UseVisualStyleBackColor = true;
+            this.cryptBtn.Click += new System.EventHandler(this.cryptBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton16);
+            this.panel1.Controls.Add(this.radioButton32);
+            this.panel1.Location = new System.Drawing.Point(121, 106);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(232, 33);
+            this.panel1.TabIndex = 3;
+            // 
+            // radioButton32
+            // 
+            this.radioButton32.AutoSize = true;
+            this.radioButton32.Checked = true;
+            this.radioButton32.Location = new System.Drawing.Point(13, 9);
+            this.radioButton32.Name = "radioButton32";
+            this.radioButton32.Size = new System.Drawing.Size(47, 16);
+            this.radioButton32.TabIndex = 0;
+            this.radioButton32.TabStop = true;
+            this.radioButton32.Text = "32位";
+            this.radioButton32.UseVisualStyleBackColor = true;
+            // 
+            // radioButton16
+            // 
+            this.radioButton16.AutoSize = true;
+            this.radioButton16.Location = new System.Drawing.Point(157, 9);
+            this.radioButton16.Name = "radioButton16";
+            this.radioButton16.Size = new System.Drawing.Size(47, 16);
+            this.radioButton16.TabIndex = 1;
+            this.radioButton16.Text = "16位";
+            this.radioButton16.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radioButtonUp);
+            this.panel2.Controls.Add(this.radioButtonLow);
+            this.panel2.Location = new System.Drawing.Point(121, 166);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(232, 33);
+            this.panel2.TabIndex = 4;
+            // 
+            // radioButtonUp
+            // 
+            this.radioButtonUp.AutoSize = true;
+            this.radioButtonUp.Location = new System.Drawing.Point(157, 8);
+            this.radioButtonUp.Name = "radioButtonUp";
+            this.radioButtonUp.Size = new System.Drawing.Size(47, 16);
+            this.radioButtonUp.TabIndex = 1;
+            this.radioButtonUp.Text = "大写";
+            this.radioButtonUp.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonLow
+            // 
+            this.radioButtonLow.AutoSize = true;
+            this.radioButtonLow.Checked = true;
+            this.radioButtonLow.Location = new System.Drawing.Point(13, 8);
+            this.radioButtonLow.Name = "radioButtonLow";
+            this.radioButtonLow.Size = new System.Drawing.Size(47, 16);
+            this.radioButtonLow.TabIndex = 0;
+            this.radioButtonLow.TabStop = true;
+            this.radioButtonLow.Text = "小写";
+            this.radioButtonLow.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "选择位数：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "选择大小写：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "加密结果：";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.resultLabel.ForeColor = System.Drawing.Color.Red;
+            this.resultLabel.Location = new System.Drawing.Point(119, 280);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(67, 14);
+            this.resultLabel.TabIndex = 8;
+            this.resultLabel.Text = "显示结果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 357);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cryptBtn);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "md5Test";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button cryptBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton16;
+        private System.Windows.Forms.RadioButton radioButton32;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radioButtonUp;
+        private System.Windows.Forms.RadioButton radioButtonLow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ md5Test/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Security.Cryptography;
+
+namespace md5Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cryptBtn_Click(object sender, EventArgs e)
+        {
+            string str = inputTextBox.Text;
+            if (str.Length < 1)
+            {
+                MessageBox.Show("请先输入需要加密的字符串");
+                return;
+            }
+
+            int bits = radioButton32.Checked ? 32 : 16;
+            string type = radioButtonLow.Checked ? "low" : "up";
+            resultLabel.Text = md5Crypt(str, bits, type);
+        }
+
+        /// <summary>
+        /// md5加密
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="bits"></param> 32或者16
+        /// <param name="type"></param> low表示小写，up表示大写
+        /// <returns></returns>
+        public static string md5Crypt(string str, int bits, string type)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] buffer = Encoding.Default.GetBytes(str);
+            byte[] encryptedBuffer = md5.ComputeHash(buffer);
+            string temStr = "";
+            string paramStr = "x2";
+            if (type == "up")
+            {
+                paramStr = "X2";
+            }
+            for (int i = 0; i < encryptedBuffer.Length; i++)
+            {
+                temStr += encryptedBuffer[i].ToString(paramStr);
+            }
+
+            if (bits == 16)
+            {
+                temStr = temStr.Substring(8, 16);
+            }
+            return temStr;
+        }
+    }
+}
